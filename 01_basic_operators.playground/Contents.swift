ddc981d822ac6218e0d244c2d4b5df7ec0198eb2
KAGA Arsene <kaga@KAGAui-MacBook.local>
2017-04-10 20:21:18 +0900

    add basic operators

---
+++ 01_basic_operators.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+str
+
+var number1 = 1
+number1 += 3
+
+let number2 = 4
+//number2 = 5
+
+2 + 3
+3 + 7
+2100 * 2
+144 / 12
+
+7 / 2
+7 % 2
+
+
